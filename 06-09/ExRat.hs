e491352806c6c834fed113d1172f3f6d465ec48e
Dante <danteaugusto.10@gmail.com>
2022-10-03 22:42:55 -0300

    ExRats done

--- 06-09/ExRat.hs
+++ 06-09/ExRat.hs
@@ -5,34 +5,50 @@ module ExRat
     , numerator
     ) where
 -- define Rat:
-data Rat = Int Int 
+data Rat = Rat Integer Integer
 
 instance Show Rat where
-    show = undefined
+    show (Rat 0 0) = "NaN"
+    show (Rat _ 0) = "Infinity"
+    show (Rat x y) = show x++"/"++ show y
 
 instance Eq Rat where
-    (==) = undefined
+    (==)(Rat x y) (Rat c d)
+        | x == c && y == d = True
+        | x > c && y > d = if (div x c == (div y d)) && (mod x c == 0) && (mod y d == 0) then True else False
+        | c > x && d > y = if (div c x == (div d y)) && (mod c x == 0) && (mod d y == 0) then True else False
+        | otherwise = False
 
 instance Num Rat where
-    (+) = undefined
-    (*) = undefined
-    negate = undefined
-    abs = undefined
-    signum = undefined
-    fromInteger = undefined
+    (+) :: Rat -> Rat -> Rat
+    (+) (Rat x y) (Rat c d)= (Rat (x*d + c*y) (y*d))
+    (*) :: Rat -> Rat -> Rat
+    (*) (Rat x y) (Rat c d)= (Rat (x*c) (y*d))
+    negate :: Rat -> Rat
+    negate (Rat x y)= (Rat (x*(-1)) y)
+    abs :: Rat -> Rat
+    abs (Rat x y)= if signum (Rat x y) == 1 then Rat x y else Rat (x*(-1)) y
+    --signum :: Rat -> Integer
+    signum (Rat x y)
+        | y == 0 && x == 0 = Rat 0 0
+        | y == 0 = if x > 0 then 1 else -1
+        | x == 0 = 0
+        | otherwise = if x*y > 0 then 1 else -1
+    fromInteger :: Integer -> Rat
+    fromInteger x = (Rat x 1)
 
 instance Ord Rat where
-    compare = undefined
+    compare (Rat x y) (Rat c d) = if (x*d) <= (c*y) then if (x*d) == (c*y) then EQ else LT else GT
 
 rat :: Integer -> Integer -> Rat
-rat = undefined
+rat x y = (Rat x y)
 
 (//) :: Rat -> Rat -> Rat
-(//) = undefined
+(//) (Rat x y) (Rat c d) = (Rat x y) (Rat d c)
 
 denominator :: Rat -> Integer
-denominator = undefined
+denominator (Rat _ x ) = x
 
 numerator :: Rat -> Integer
-numerator = undefined
+numerator (Rat x _ ) = x
 
